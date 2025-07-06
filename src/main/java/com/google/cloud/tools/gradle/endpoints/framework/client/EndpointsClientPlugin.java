51c333a4c16b98d080cd4150164d31a3659cb9c4
Magnus Wahlgren <magnus.wahlgren@golfisverige.com>
2025-07-06 02:09:30 +0200

    [Gradle Release Plugin] - pre tag commit:  'v2.1.9'.

--- src/main/java/com/google/cloud/tools/gradle/endpoints/framework/client/EndpointsClientPlugin.java
+++ src/main/java/com/google/cloud/tools/gradle/endpoints/framework/client/EndpointsClientPlugin.java
@@ -1,19 +1,3 @@
-/*
- *  Copyright (c) 2016 Google Inc. All Right Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.cloud.tools.gradle.endpoints.framework.client;
 
 import com.google.cloud.tools.gradle.endpoints.framework.client.task.ExtractDiscoveryDocZipsTask;
@@ -24,7 +8,6 @@ import com.google.common.collect.ImmutableMap;
 import java.io.File;
 import java.util.Collection;
 import org.gradle.api.Action;
-import org.gradle.api.NonNullApi;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPluginConvention;
